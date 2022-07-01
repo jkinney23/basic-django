97fbed46c4d81ebe6b6d0f6ccb08219944c8bd27
Jason Kinney <jkinney23@yahoo.ca>
2022-07-01 12:36:46 -0700

    add webpack and tailwind CSS

--- basic_django/settings.py
+++ basic_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
+    'webpack_boilerplate',
     'allauth',
     'allauth.account',
-#    'allauth.socialaccount',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -143,6 +145,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 
+    'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath("frontend/build"),
+]
+
+WEBPACK_LOADER = {
+    "MANIFEST_FILE": BASE_DIR.joinpath("frontend/build/manifest.json"),
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
